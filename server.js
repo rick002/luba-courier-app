37bb6b63112de2bded7b0e1c507bd52d97a25d74
Jose Ricardo Osorio Soto <osorioj842@gmail.com>
2021-06-29 05:59:53 -0400

    index.html title changed

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const path = require('path');
 const express = require('express');
+const proxy = require('express-http-proxy');
+
 const app = express();
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/luba'));
+
+// To Proxy every URL starting with /api
+
+const apiProxy = proxy('https://courierdemo.azurewebsites.net/api');
+
+app.use('/api/*', apiProxy);
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
